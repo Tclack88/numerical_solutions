b16552c57ebc926b817af43b6291a7b83ba08804
Tclack88 <tclack@aol.com>
2022-06-27 14:24:16 +1000

    made animated plot

--- swinging_atwood/atwood.py
+++ swinging_atwood/atwood.py
@@ -1,23 +1,13 @@
 import matplotlib.pyplot as plt
 import matplotlib
+from matplotlib import animation
+from matplotlib.animation import FuncAnimation
 import numpy as np
 from numpy import pi, cos, sin
 from random import random as rand
 import scipy
 from scipy.integrate import odeint, solve_ivp
 
-#
-# plt.gca().axes.get_xaxis().set_visible(False)
-# plt.axis('off')
-# #plt.axes(projection = 'polar')
-# r = 3
-# o = pi/2
-# plt.polar(o,0,'g.',o,r,'g.')
-# plt.show()
-
-global m
-global M
-global g
 g = 9.807
 m = 1
 M = 7
@@ -25,7 +15,7 @@ L = 1 # length of rope
 d = 1 # distance between pulleys
 
 def derivatives(t,y):
-    # y = [r, r', o, o']
+    # Expected input: y = [r, r', o, o']
     r = y[0:2]
     o = y[2:4]
     r_derivs = [r[1], (m*r[0]*o[1]**2 + g*(m*cos(o[0])-M))/(m+M)] # r' and r''
@@ -33,20 +23,55 @@ def derivatives(t,y):
     dydt =  np.array(r_derivs + o_derivs) # concat derivs and return array
     return dydt
 
-#x0 = [1, pi/3] # initial conditions. Radius, angle
-y0 = np.array([1, 0, pi/6, 0]) # initial conditions
+y0 = np.array([1, 0, pi/6, 0]) # initial conditions (r, r', o, o')
 t_range = np.linspace(0,10,num=1001)
+plot_range = range(len(t_range))
 
 sol = solve_ivp(derivatives, [t_range[0],t_range[-1]], y0, method='Radau', t_eval=t_range)
 
-#plt.plot(t_range,sol.y[1])
-#plt.polar(o,0,'g.',o,r,'g.')
 rs = sol.y[0]
 os = sol.y[2]
 rs2 = L - rs
 os2 = -pi/2*np.ones(np.shape(rs2))
 
 os = os - pi/2
-plt.polar(os, rs,'g',os2,rs2,'r')
+
+dat = [rs,os]
+
+fig, ax = plt.subplots(figsize =(6,6))
+ax.set(xlim = (-2,2), ylim = (-2,2))
+x = rs*cos(os)
+y = rs*sin(os)
+x2 = rs2*cos(os2) - 1
+y2 = rs2*sin(os2)
+
+
+def animate(i):
+    lim = 100 # set how amny prevous points will be included in the motion
+    ax.clear()
+    ax.set(xlim = (-2,2), ylim = (-2,2))
+    # Plot the motion of the "main mass"
+    if i <= lim:
+        ax.plot(x[:i],y[:i])
+    else:
+        ax.plot(x[i-lim:i],y[i-lim:i])
+    ax.plot([0,x[i-1]],[0,y[i-1]],lw=1,c='blue')
+    ax.plot(x[i],y[i],c='blue',marker='o')
+    # plot the motion of the other mass
+    ax.plot(x2[i],y2[i], marker='o', c='blue')
+    ax.plot([-1,x2[i-1]],[0,y2[i-1]],lw=1,c='blue')
+    # Plot pulleys and connecting rope
+    ax.plot(0,0, marker='o',c='black')
+    ax.plot(-1,0, marker='o',c='black')
+    ax.plot([-1,0],[0,0], c='blue',lw=1)
+
+
+anim = FuncAnimation(fig, animate, interval=5, frames=len(t_range)-1)
+
+plt.draw()
 plt.show()
 
+
+#f = r"atwood.gif"
+#writergif = animation.PillowWriter(fps=60)
+#anim.save(f, writer=writergif)
